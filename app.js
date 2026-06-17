6292bf39d59c88fea171f48a8a2ec41ace9d7a11
Edmund Valdoz <edmundmarievaldoz@gmail.com>
2026-06-17 22:59:17 +0100

    set up a server to talk to browser

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-//imports -----------------------------------
+// imports -----------------------------------
+import express from 'express';
 
 // configure express app -------------------
+const app = new express();
+
 
 // configure middleware --------------------
 
 // endpoints -------------------------------
 
 // start server ----------------------------
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
