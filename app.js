e6998f27386f8d8f7c5ca060ca6c7d181efcc3ad
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-06-22 23:25:47 +0100

    update app.js to connect backend and front end together

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const app = new express();
 
 // configure middleware --------------------
 app.use(express.json());
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // controllers -----------------------------
 
